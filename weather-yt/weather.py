21d35434896ef1ca5057a82b17dbb69516b6f088
Egor Chichkin <echichkin84@gmail.com>
2025-11-17 12:03:57 +0300

    Перед проверкой работы приложения

--- weather-yt/weather.py
+++ weather-yt/weather.py
@@ -1,11 +1,20 @@
+from exceptions import ApiServiceError, CantGetCoordinates
 from coordinates import get_gps_coordinates
 from weather_api_service import get_weather
 from weather_formatter import format_weather
 
 
 def main():
-    coordinates = get_gps_coordinates()
-    weather = get_weather(coordinates)
+    try:
+        coordinates = get_gps_coordinates()
+    except CantGetCoordinates:
+        print("Can't get GPS coordinates")
+        exit(1)
+    try:
+        weather = get_weather(coordinates)
+    except ApiServiceError:
+        print("Can't get weather by API")
+        exit(1)
     formatted_weather = format_weather(weather)
     print(formatted_weather)
 
--- weather-yt/weather_api_service.py
+++ weather-yt/weather_api_service.py
@@ -1,9 +1,16 @@
 from dataclasses import dataclass # подключаем декоратор для лаконичного объявления классов с данными.
 from datetime import datetime 
-from typing import TypeAlias #добавляем поддержку псевдонимов, чтобы делать типы кода более понятными.
+from typing import TypeAlias, Literal #добавляем поддержку псевдонимов, чтобы делать типы кода более понятными.
 from enum import Enum
+import json
+from json.decoder import JSONDecodeError
+import ssl
+import urllib.request
+from urllib.error import URLError
+
 from coordinates import Coordinates # импортируем класс координат из файла coordinates.py
 import config
+from exceptions import ApiServiceError
 
 Celsius: TypeAlias = int # теперь если в аннотациях указано Celsius, ясно, что это именно температура в градусах °C
 
@@ -27,11 +34,67 @@ class Weather: # класс, который будет содержать дан
     city: str
 
 def get_weather(coordinates: Coordinates) -> Weather:
-    """Requests weather data from the API and returns it"""
-    return Weather(
-        temperature=20,
-        weather_type=WeatherType.CLEAR,
-        sunrise=datetime.fromisoformat('2022-05-04 04:00:00'),
-        sunset=datetime.fromisoformat('2022-05-04 20:25:00'),
-        city='Moscow'
+    """Requests weather from OpenWeather API and returns it"""
+    openweather_response = _get_openweather_response(
+        latitude = coordinates.latitude,
+        longitude = coordinates.longitude
     )
+    weather = _parse_openweather_respose(openweather_response)
+    return weather
+
+def _get_openweather_response(latitude: float, longitude: float) -> str:
+    ssl._create_default_https_context = ssl._create_unverified_context
+    url = config.OPENWEATHER_URL.format(latitude=latitude, longitude=longitude)
+    try:
+        return urllib.request.urlopen(url).read()
+    except URLError:
+        raise ApiServiceError("Request error to OpenWeather")
+
+def _parse_openweather_respose(openweather_response: str) -> Weather:
+    try:
+        openweather_dict = json.loads(openweather_response)
+    except JSONDecodeError:
+        raise ApiServiceError("Error answer decoding")
+    
+    return Weather(
+        temperature=_parse_temperature(openweather_dict),
+        weather_type=_parse_weather_type(openweather_dict),
+        sunrise=_parse_sun_time(openweather_dict, "sunrize"),
+        sunset=_parse_sun_time(openweather_dict, "sunset"),
+        city=_parse_city(openweather_dict)
+    )
+
+def _parse_temperature(openweather_dict: dict) -> Celsius:
+    return round(openweather_dict["main"]["temp"])
+
+def _parse_weather_type(openweather_dict: dict) -> WeatherType:
+    try:
+        weather_type_id = str(openweather_dict["weather"][0]["id"])
+    except (IndexError, KeyError):
+        raise ApiServiceError("Weather type parsing error")
+    weather_types = {
+        "1": WeatherType.THUNDERSTORM,
+        "3": WeatherType.DRIZZLE,
+        "5": WeatherType.RAIN,
+        "6": WeatherType.SNOW,
+        "7": WeatherType.FOG,
+        "800": WeatherType.CLEAR,
+        "80": WeatherType.CLOUDS
+    }
+    for _id, _weather_type in weather_types.items():
+        if weather_type_id.startswith(_id):
+            return _weather_type
+    raise ApiServiceError("Unknown weather type")
+
+def _parse_sun_time(openweather_dict: dict, time: Literal["sunrise"] | Literal["sunset"]) -> datetime:
+    return datetime.fromtimestamp(openweather_dict["sys"][time])
+
+def _parse_city(openweather_dict: dict) -> str:
+    return openweather_dict["name"]
+
+if __name__ == "__main__":
+    coords = Coordinates(latitude=55.7, longitude=37.6)
+    try:
+        print(get_weather(coords))
+    except ApiServiceError as err:
+        print(f"Error: {err}")
